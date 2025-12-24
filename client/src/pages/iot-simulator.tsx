7eb275d07af31fdaa1e2770092065e02509dbfdb
tridevbalaji <tridev.2416@gmail.com>
2025-12-24 23:09:51 +0530

    temperature sensor completed

--- client/src/pages/iot-simulator.tsx
+++ client/src/pages/iot-simulator.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import mqtt from 'mqtt';
 import { DashboardLayout } from '../components/iot-simulation/DashboardLayout';
 import { Header } from '../components/iot-simulation/Header';
 import { SensorSidebar } from '../components/iot-simulation/SensorSidebar';
@@ -8,14 +9,53 @@ import { useSensorStore } from '../components/iot-simulation/store';
 
 export default function IoTSimulatorPage() {
   const updateSensorValues = useSensorStore(state => state.updateSensorValues);
+  const updateTemperatureFromMQTT = useSensorStore(state => state.updateTemperatureFromMQTT);
 
   useEffect(() => {
+    // MQTT Connection for Temperature Sensor
+    const brokerUrl = "wss://broker.hivemq.com:8884/mqtt";
+    const options = {
+      clientId: "webclient_" + Math.random().toString(16).substr(2, 8),
+      clean: true,
+      connectTimeout: 4000,
+      reconnectPeriod: 1000
+    };
+
+    const client = mqtt.connect(brokerUrl, options);
+
+    client.on("connect", function() {
+      console.log("MQTT Connected");
+      client.subscribe("esp32/dht/temperature");
+    });
+
+    client.on("message", function(topic, message) {
+      try {
+        const data = JSON.parse(message.toString());
+        console.log(data);
+
+        if (data.status === "ok" && typeof data.temperature === 'number') {
+          // Update ONLY the Temperature sensor value
+          updateTemperatureFromMQTT(data.temperature);
+        }
+      } catch (error) {
+        console.error("Error parsing MQTT message:", error);
+      }
+    });
+
+    client.on("error", function(error) {
+      console.log("MQTT Error:", error);
+    });
+
+    // Update other sensors (not Temperature) every second
     const interval = setInterval(() => {
       updateSensorValues();
-    }, 1000); // Update every second
+    }, 1000);
 
-    return () => clearInterval(interval);
-  }, [updateSensorValues]);
+    return () => {
+      client.end();
+      clearInterval(interval);
+    };
+  }, [updateSensorValues, updateTemperatureFromMQTT]);
 
   return (
     <DashboardLayout>
